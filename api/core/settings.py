0a5eb52d4c7fe407c14a08b3c98b120e927a26e8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-07-11 17:50:18 -0700

    refactor(core-dev-settings): move settings for dev environment

--- api/core/settings.py
+++ api/core/settings.py
@@ -1,24 +1,10 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@pws2)=_mca+!qmlt@pl%ub3ir7n7kuiyma!u$2s62(2@vo6p-'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 
---
+++ api/core/settings/dev.py
@@ -0,0 +1,37 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = (
+  'django-insecure-@pws2)=_mca+!qmlt@pl%ub3ir7n7kuiyma!u$2s62(2@vo6p-',
+  )
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*']
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PATCH",
+    "PUT",
+]
