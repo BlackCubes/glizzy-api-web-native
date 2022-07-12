3ab193581a8498cbacd8b7aabc6e0aeef24d94f9
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-07-12 10:34:53 -0700

    feat(glizzy): initialize new app

--- api/core/settings/base.py
+++ api/core/settings/base.py
@@ -24,6 +24,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # Application definition
 
 INSTALLED_APPS = [
+    "glizzy",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
