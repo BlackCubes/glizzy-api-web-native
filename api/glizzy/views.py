c4cf5cd5636a6fc5da40c728dee2dd12cd6d41ae
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-07-13 12:45:13 -0700

    feat(glizzy-views): create a retrieve view
    NOTE: Only for GET requests

--- api/glizzy/views.py
+++ api/glizzy/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import Glizzy
+from .serializers import GlizzySerializer
+
+from core.mixins import MultipleFieldLookupMixin
+
+
+class RetrieveGlizzyView(MultipleFieldLookupMixin, generics.RetrieveAPIView):
+    """
+    A generic view that retrieves all objects from the Glizzy model, or finds
+    a particular object given either a ``pk``  or ``slug`` from the URL.
+
+    NOTE: Only a GET request.
+    """
+
+    permission_classes = (permissions.AllowAny,)
+    queryset = Glizzy.objects.all()
+    serializer_class = GlizzySerializer
+    lookup_fields = (
+        "pk",
+        "slug",
+    )
