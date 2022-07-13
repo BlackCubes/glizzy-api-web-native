568a3becf3875fe650b640f205dbe3b88bb66d21
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-07-13 13:27:04 -0700

    fix(glizzy-urls): fix invalid converter

--- api/glizzy/urls.py
+++ api/glizzy/urls.py
@@ -7,6 +7,6 @@ app_name = "glizzy"
 
 urlpatterns = [
     path("", views.GlizzyListView.as_view(), name="list"),
-    path("<pk:pk>", views.GlizzyDetailView.as_view(), name="detail"),
+    path("<int:pk>", views.GlizzyDetailView.as_view(), name="detail"),
     path("<slug:slug>", views.GlizzyDetailView.as_view(), name="detail"),
 ]
