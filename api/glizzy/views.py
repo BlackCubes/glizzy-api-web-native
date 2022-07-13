d2ee9cd01b2828623456f9b1b5ed24f37f236316
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-07-13 13:11:46 -0700

    feat(glizzy-views): create a list view and update docs

--- api/glizzy/views.py
+++ api/glizzy/views.py
@@ -6,10 +6,22 @@ from .serializers import GlizzySerializer
 from core.mixins import MultipleFieldLookupMixin
 
 
+class GlizzyListView(generics.ListAPIView):
+    """
+    A generic view that finds all objects from the Glizzy model.
+
+    NOTE: Only a GET request.
+    """
+
+    permission_classes = (permissions.AllowAny,)
+    queryset = Glizzy.objects.all()
+    serializer_class = GlizzySerializer
+
+
 class GlizzyDetailView(MultipleFieldLookupMixin, generics.RetrieveAPIView):
     """
-    A generic view that finds a particular object given either a ``pk`` or
-    ``slug`` from the URL, if it exists.
+    A generic view that finds a particular object from the Glizzy model given
+    either a ``pk`` or ``slug`` from the URL, if it exists.
 
     NOTE: Only a GET request.
     """
