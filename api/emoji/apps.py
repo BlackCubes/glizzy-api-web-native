cf2cc544dc4daaa08da111bacbba6b0c0f318826
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-07-14 12:48:24 -0700

    feat(emoji-apps): ready the signals

--- api/emoji/apps.py
+++ api/emoji/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EmojiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "emoji"
+
+    def ready(self):
+        from . import signals
