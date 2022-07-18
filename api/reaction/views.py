d619e13fd68121b7bfd1afe2f26863fc450706f6
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-07-18 12:56:32 -0700

    feat(reaction-views): create retrieve/create view

--- api/reaction/views.py
+++ api/reaction/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import Reaction
+from .serializers import ReactionSerializer
+
+from core.utils import final_success_response
+
+
+class ReactionListCreateView(generics.ListCreateAPIView):
+    """
+    A generic view that finds all objects from the Reaction model.
+
+    NOTE: Only a GET and POST request.
+    """
+
+    permission_classes = (permissions.AllowAny,)
+    queryset = Reaction.objects.all().order_by("reaction_count")
+    serializer_class = ReactionSerializer
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        final_success_response(request, response)
+
+        return super().finalize_response(request, response, *args, **kwargs)
