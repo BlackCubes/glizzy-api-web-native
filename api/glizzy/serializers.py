428a4ff37e3c1701981315528768d172f1c89a62
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-07-18 21:34:13 -0700

    feat(glizzy-serializers): add reactions to the output

--- api/glizzy/serializers.py
+++ api/glizzy/serializers.py
@@ -2,15 +2,20 @@ from rest_framework import serializers
 
 from .models import Glizzy
 
+from reaction.serializers import ReactionSerializer
+
 
 class GlizzySerializer(serializers.ModelSerializer):
     """
     A ModelSerializer that maps all of the Glizzy model fields with the given
     serializer fields of ``id``, ``uuid``, ``name``, ``slug``, ``short_info``,
-    ``long_info``, ``image``, ``created_at``, and ``updated_at``.
+    ``long_info``, ``image``, ``reactions``, ``created_at``, and
+    ``updated_at``.
 
     NOTE: All Django models have an ``id`` field by default upon creation.
     """
+
+    reactions = ReactionSerializer(many=True, read_only=True)
 
     class Meta:
         model = Glizzy
@@ -22,6 +27,18 @@ class GlizzySerializer(serializers.ModelSerializer):
             "short_info",
             "long_info",
             "image",
+            "reactions",
             "created_at",
             "updated_at",
         )
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        if representation.get("reactions", None):
+            for reaction in representation["reactions"]:
+                reaction.pop("glizzy")
+                reaction.pop("created_at")
+                reaction.pop("updated_at")
+
+        return representation
