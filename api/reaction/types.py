4ba4a4241a867b9d7d5f8809205331a6257374f1
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-07-21 20:50:55 -0700

    feat(reaction-types): extend the type to be used in the 'glizzy' app

--- api/reaction/types.py
+++ api/reaction/types.py
@@ -4,16 +4,32 @@ import typing
 import uuid
 
 
-@strawberry.type
-class Reaction:
+@strawberry.type(
+    description="A base level type without the ``glizzy`` field. Also being shared when mutating ``Reaction`` which has the ``glizzy`` field."
+)
+class BaseReaction:
     """
-    A GraphQL type representing the Reaction model.
+    A GraphQL type representing most of the fields in the Reaction model
+    except for the ``glizzy`` field. This is because this type is being shared
+    in the ``reaction`` and ``glizzy`` apps. The ``reaction`` app needs the
+    ``glizzy`` field to be displayed while the ``glizzy`` app does not (to
+    avoid redundancy).
     """
 
     id: strawberry.ID
     uuid: uuid.UUID
     reaction_count: int
     emoji: str
+    created_at: typing.Optional[datetime.datetime]
+    updated_at: typing.Optional[datetime.datetime]
+
+
+@strawberry.type(
+    description="A ``Reaction`` type implementing ``BaseReaction`` (``BaseReaction`` is being used when querying ``Glizzy``). After mutation, all the fields from ``BaseReaction`` gets returned with the ``glizzy`` field from ``Reaction`` (``BaseReaction`` does not have the ``glizzy`` field)."
+)
+class Reaction(BaseReaction):
+    """
+    A GraphQL type representing the Reaction model with the ``glizzy`` field.
+    """
+
     glizzy: str
-    created_at: typing.Optional[datetime.datetime] = None
-    updated_at: typing.Optional[datetime.datetime] = None
