e7225d014be735a67192e7da1c9dfc9504cec033
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-07-22 21:28:54 -0700

    docs(reaction-schema): add extra mutation description

--- api/reaction/schema.py
+++ api/reaction/schema.py
@@ -13,5 +13,5 @@ class Mutation:
 
     add_reaction: typing.Optional[types.Reaction] = strawberry.field(
         resolver=resolvers.add_reaction,
-        description="Execute mutation of ``addReaction`` that will return one reaction or none. Must use arguments of ``emoji``, ``glizzy``, and ``reactionCount``. Errors will occur if the ``reactionCount`` is not 0 or greater; both the ``emoji`` and ``glizzy`` are empty strings; or both ``emoji`` and ``glizzy`` could not be found in the database.",
+        description="Execute mutation of ``addReaction`` that will return one reaction or none. Must use arguments of ``emoji``, ``glizzy``, and ``reactionCount``. ``emoji`` must be the an actual emoji (i.e. 🌭), and the ``glizzy`` must be the name of the frank (i.e. 'Chicago Dog'). Errors will occur if the ``reactionCount`` is not 0 or greater; both the ``emoji`` and ``glizzy`` are empty strings; or both ``emoji`` and ``glizzy`` could not be found in the database.",
     )
