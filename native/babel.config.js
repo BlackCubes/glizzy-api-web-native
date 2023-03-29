793beb6b03db18003067408b0c1b5705d33910ee
Elias Gutierrez <gutierrezelias1991@gmail.com>
2023-03-29 11:30:48 -0700

    style(eslint): run eslint for testing

--- native/babel.config.js
+++ native/babel.config.js
@@ -1,16 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
       [
-        "module-resolver",
+        'module-resolver',
         {
-          root: ["./src"],
-          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
           alias: {
-            tests: ["./tests/"],
-            "@components": "./src/components",
+            tests: ['./tests/'],
+            '@components': './src/components',
           },
         },
       ],
